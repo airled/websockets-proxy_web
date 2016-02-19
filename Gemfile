85f8bd2d8b2ea48b7234bb244f95f751019a8c40
airled <airled@mail.ru>
2016-02-19 20:12:29 +0300

    mina

--- Gemfile
+++ Gemfile
@@ -34,3 +34,5 @@ gem 'padrino', '0.13.1'
 # %w(core support gen helpers cache mailer admin).each do |g|
 #   gem 'padrino-' + g, '0.13.1'
 # end
+
+gem 'mina'
---
+++ config/deploy.rb
@@ -0,0 +1,50 @@
+require 'mina/bundler'
+require 'mina/git'
+require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
+
+set :domain, '51.254.10.211'
+set :deploy_to, '/home/wsweb'
+set :repository, 'https://github.com/airled/websockets-proxy_web'
+set :branch, 'master'
+set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log']
+set :user, 'wsweb'    # Username in the server to SSH to.
+
+task :environment do
+  invoke :'rbenv:load'
+end
+
+task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/config"]
+  queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
+  queue! %[touch "#{deploy_to}/#{shared_path}/config/secrets.yml"]
+  queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/database.yml' and 'secrets.yml'."]
+
+  if repository
+    repo_host = repository.split(%r{@|://}).last.split(%r{:|\/}).first
+    repo_port = /:([0-9]+)/.match(repository) && /:([0-9]+)/.match(repository)[1] || '22'
+    queue %[
+      if ! ssh-keygen -H  -F #{repo_host} &>/dev/null; then
+        ssh-keyscan -t rsa -p #{repo_port} -H #{repo_host} >> ~/.ssh/known_hosts
+      fi
+    ]
+  end
+end
+
+desc "Deploys the current version to the server."
+task :deploy => :environment do
+  to :before_hook do
+  end
+  deploy do
+    invoke :'git:clone'
+    invoke :'deploy:link_shared_paths'
+    invoke :'bundle:install'
+    invoke :'deploy:cleanup'
+    to :launch do
+      queue "mkdir -p #{deploy_to}/#{current_path}/tmp/"
+      queue "touch #{deploy_to}/#{current_path}/tmp/restart.txt"
+    end
+  end
+end
