f96a01392303f2cd730dabfd987e8e741badd210
airled <airled@mail.ru>
2016-02-22 16:56:49 +0300

    active

--- db/migrate/001_create_accounts.rb
+++ db/migrate/001_create_accounts.rb
@@ -2,12 +2,13 @@ Sequel.migration do
   up do
     create_table :accounts do
       primary_key :id
-      String :email, :unique => true
+      String :email, unique: true
       String :crypted_password
       String :role
       Boolean :confirmed, default: false
-      String :queue
-      Integer :port
+      Boolean :active, default: false
+      String :queue, unique: true, default: nil
+      Integer :port, unique: true, default: nil
     end
   end
 
